aeddbe9a75eabbc48b5b68fa2fb199e162578519
1027 <1027@BIT05-07>
2019-02-19 17:30:52 +0900

    암호화까지

--- mvcopenproject/src/main/java/com/bitcamp/openp/interceptor/AuthCheckInterceptor.java
+++ mvcopenproject/src/main/java/com/bitcamp/openp/interceptor/AuthCheckInterceptor.java
@@ -17,12 +17,11 @@ public class AuthCheckInterceptor extends HandlerInterceptorAdapter{
 		if(session != null) {
 			Object authObj = session.getAttribute("loginInfo");
 			if(authObj != null) { // 세션 있고, 로그인 되어있을 경우
-				System.out.println("(로그인 상태) 정상 처리");
+				response.sendRedirect(request.getContextPath()+"/member/memberLogin");
 				return true;				
 			}
 		}
-		
-		System.out.println("비로그인 상태");
+
 
 		response.sendRedirect(request.getContextPath()+"/member/loginForm");
 		
